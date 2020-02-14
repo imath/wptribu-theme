02196f7393d3688450b35872a37cd65912bfc1cd
imath <math.viet@gmail.com>
2020-02-14 03:49:51 +0100

    Remove WP.org specific functions & domain name

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -30,7 +30,7 @@ if ( ! function_exists( __NAMESPACE__ . '\entry_meta' ) ) :
 			// phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
 			printf(
 				/* translators: 1: post date 2: post author */
-				'<span class="posted-on">' . __( 'Posted on %1$s by %2$s.', 'wporg' ) . '</span>',
+				'<span class="posted-on">' . __( 'Posted on %1$s by %2$s.', 'wptribu' ) . '</span>',
 				$time_string,
 				$author_string
 			);
@@ -41,7 +41,7 @@ if ( ! function_exists( __NAMESPACE__ . '\entry_meta' ) ) :
 		if ( current_theme_supports( 'post-formats', $format ) ) {
 			printf(
 				'<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
-				sprintf( '<span class="screen-reader-text">%s </span>', esc_html_x( 'Format', 'Used before post format.', 'wporg' ) ),
+				sprintf( '<span class="screen-reader-text">%s </span>', esc_html_x( 'Format', 'Used before post format.', 'wptribu' ) ),
 				esc_url( get_post_format_link( $format ) ),
 				esc_html( get_post_format_string( $format ) )
 			);
@@ -55,7 +55,7 @@ if ( ! function_exists( __NAMESPACE__ . '\entry_meta' ) ) :
 			echo '<span class="comments-link">';
 			comments_popup_link( sprintf(
 				/* translators: Post title. */
-				__( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'wporg' ),
+				__( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'wptribu' ),
 				get_the_title()
 			) );
 			echo '</span>';
@@ -95,7 +95,7 @@ if ( ! function_exists( __NAMESPACE__ . '\entry_date' ) ) :
 	function entry_date() {
 		printf(
 			'<span class="posted-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></span>',
-			esc_html_x( 'Posted on', 'Used before publish date.', 'wporg' ),
+			esc_html_x( 'Posted on', 'Used before publish date.', 'wptribu' ),
 			esc_url( get_permalink() ),
 			get_entry_date() // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
 		);
@@ -109,71 +109,22 @@ if ( ! function_exists( __NAMESPACE__ . '\entry_taxonomies' ) ) :
 	 * Create your own WordPressdotorg\Theme\entry_taxonomies() function to override in a child theme.
 	 */
 	function entry_taxonomies() {
-		$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'wporg' ) );
+		$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'wptribu' ) );
 		if ( $categories_list && categorized_blog() ) {
 			printf(
 				'<span class="cat-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
-				esc_html_x( 'Categories', 'Used before category names.', 'wporg' ),
+				esc_html_x( 'Categories', 'Used before category names.', 'wptribu' ),
 				$categories_list // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
 			);
 		}
 
-		$tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'wporg' ) );
+		$tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'wptribu' ) );
 		if ( $tags_list ) {
 			printf(
 				'<span class="tags-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
-				esc_html_x( 'Tags', 'Used before tag names.', 'wporg' ),
+				esc_html_x( 'Tags', 'Used before tag names.', 'wptribu' ),
 				$tags_list // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
 			);
 		}
 	}
 endif;
-
-if ( ! function_exists( __NAMESPACE__ . '\categorized_blog' ) ) :
-	/**
-	 * Determines whether blog/site has more than one category.
-	 *
-	 * Create your own  WordPressdotorg\Theme\categorized_blog() function to override in a child theme.
-	 *
-	 * @return bool True if there is more than one category, false otherwise.
-	 */
-	function categorized_blog() {
-		$all_the_cool_cats = get_transient( 'wporg_categories' );
-
-		if ( false === $all_the_cool_cats ) {
-			// Create an array of all the categories that are attached to posts.
-			$all_the_cool_cats = get_categories( array(
-				'fields' => 'ids',
-				// We only need to know if there is more than one category.
-				'number' => 2,
-			) );
-
-			// Count the number of categories that are attached to the posts.
-			$all_the_cool_cats = count( $all_the_cool_cats );
-
-			set_transient( 'wporg_categories', $all_the_cool_cats );
-		}
-
-		if ( $all_the_cool_cats > 1 ) {
-			// This blog has more than 1 category so wporg_categorized_blog should return true.
-			return true;
-		} else {
-			// This blog has only 1 category so wporg_categorized_blog should return false.
-			return false;
-		}
-	}
-endif;
-
-/**
- * Flushes out the transients used in wporg_categorized_blog().
- */
-function category_transient_flusher() {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	}
-
-	// Like, beat it. Dig?
-	delete_transient( 'wporg_categories' );
-}
-add_action( 'edit_category', __NAMESPACE__ . '\category_transient_flusher' );
-add_action( 'save_post', __NAMESPACE__ . '\category_transient_flusher' );
